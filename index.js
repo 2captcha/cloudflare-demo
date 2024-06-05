7b6a2635a9d6536ffc6825afc7815afbaafbab4a
dzmitry-duboyski <dzmitry.duboyski@gmail.com>
2024-06-05 17:48:33 +0400

    Add info about headless mode

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ const example = async () => {
     const preloadFile = readFileSync('./inject.js', 'utf8');
     await page.evaluateOnNewDocument(preloadFile);
 
+    // If you are using `headless: true` mode, you need to manually set your `userAgent`:
+    // await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36')
+
     // Here we intercept the console messages to catch the message logged by inject.js script
     page.on('console', async (msg) => {
         const txt = msg.text()
